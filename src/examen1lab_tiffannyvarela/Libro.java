4dd6d5dc2a480260cb55c57d7d06838bdef57553
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-02-15 15:59:27 -0600

    Primero

---
+++ src/examen1lab_tiffannyvarela/Libro.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1lab_tiffannyvarela;
+
+/**
+ *
+ * @author tiffa
+ */
+public class Libro {
+    private String titulo;
+    private String descripcion;
+    private int puntaje;
+    private int disponibles;
+    private String genero;
+    private double valor;
+    private String edicion;
+    private String autor;
+    private String publicacion;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String descripcion, int puntaje, int disponibles, String genero, double valor, String edicion, String autor, String publicacion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.puntaje = puntaje;
+        this.disponibles = disponibles;
+        this.genero = genero;
+        this.valor = valor;
+        this.edicion = edicion;
+        this.autor = autor;
+        this.publicacion = publicacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public int getDisponibles() {
+        return disponibles;
+    }
+
+    public void setDisponibles(int disponibles) {
+        this.disponibles = disponibles;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(String publicacion) {
+        this.publicacion = publicacion;
+    }
+    
+    
+    
+}
---
+++ src/examen1lab_tiffannyvarela/Socio.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1lab_tiffannyvarela;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author tiffa
+ */
+public class Socio {
+    private String usuario;
+    private String contra;
+    private Date fecha_nacimiento;
+    private String numero;
+    private String correo;
+    private String nombre;
+    private String genero;
+    private ArrayList<Libro>libros= new ArrayList();
+    private ArrayList amigos = new ArrayList();
+
+    public Socio() {
+    }
+
+    public Socio(String usuario, String contra, Date fecha_nacimiento, String numero, String correo, String nombre, String genero) {
+        this.usuario = usuario;
+        this.contra = contra;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.numero = numero;
+        this.correo = correo;
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+
+    public ArrayList getAmigos() {
+        return amigos;
+    }
+
+    public void setAmigos(ArrayList amigos) {
+        this.amigos = amigos;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Socio{" + "nombre=" + nombre + '}';
+//    }
+
+    @Override
+    public String toString() {
+        return "Socio{" + "usuario=" + usuario + ", contra=" + contra + ", fecha_nacimiento=" + fecha_nacimiento + ", numero=" + numero + ", correo=" + correo + ", nombre=" + nombre + ", genero=" + genero + ", libros=" + libros + ", amigos=" + amigos + '}';
+    }
+    
+    
+    
+    
+    
+}
